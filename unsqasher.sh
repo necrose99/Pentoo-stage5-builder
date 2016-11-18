7d1f5109624444f11f7c874467f10c822c415e0b
Michael L <mike@michaellawrenceit.com>
2016-11-18 13:00:56 -0600

    Update unsqasher.sh

--- unsqasher.sh
+++ unsqasher.sh
@@ -1,8 +1,80 @@
-http://mirror.switch.ch/ftp/mirror/pentoo/latest-iso-symlinks/pentoo-amd64-default.iso
-http://mirror.switch.ch/ftp/mirror/pentoo/latest-iso-symlinks/pentoo-amd64-hardened.iso
-http://mirror.switch.ch/ftp/mirror/pentoo/latest-iso-symlinks/pentoo-x86-default.iso
-http://mirror.switch.ch/ftp/mirror/pentoo/latest-iso-symlinks/pentoo-x86-hardened.iso
+#!/bin/bash
+####################
+#
+# file: unsqasher.sh
+#
+# Unpack Pentoo ISO's into /tmp/$dirs
+#
+# author: Necrose99
+#       %%% %%%%%%%            |#|
+#     %%%% %%%%%%%%%%%        |#|####
+#  %%%%% %         %%%       |#|=#####
+#  %%%%% %   @    @   %%      | | ==####
+# %%%%%% % (_  ()  )  %%     | |    ===##
+# %%  %%% %  \_    | %%      | |       =##
+# %    %%%% %  u^uuu %%     | |         ==#
+#       %%%% %%%%%%%%%      | |           V
+### Brought to you by Necrose99 , Put a Scythe to your problem shall we...
+#####################################################################################
 
-unsquashfs -f -d /mnt/gentoo /mnt/cdrom/image.squashfs
-unsquashfs -f -d /mnt/gentoo /mnt/cdrom/modules/portage-*.lzm
-unsquashfs -f -d /mnt/gentoo /mnt/cdrom/modules/pentoo_overlay-*.lzm
+dir[0]=/tmp/pentoo-amd64-default 
+dir[1]=/tmp/pentoo-amd64-hardened
+dir[2]=/tmp/pentoo-x86-default 
+dir[3]=/tmp/pentoo-x86-hardened
+
+#########################################################################################
+unpack_iso
+file_mover
+unsquashfs_rootfs
+file_clean
+
+################################################
+
+unpack_iso() {
+
+for (( i = 0 ; i < ${#dir[@]} ; i++ ))
+do
+        7z e -r ${ $dir[$i]}/*.iso
+done
+
+}
+
+#### move files to /temporary FS ROOT.... 
+mover () {
+for (( i = 0 ; i < ${#dir[@]} ; i++ ))
+do
+        mv  ${ $dir[$i]}/modules/*.lzm ${ $dir[$i]}
+done
+
+}
+
+unsquashfs_rootfs() {
+
+for (( i = 0 ; i < ${#dir[@]} ; i++ ))
+do
+        unsquashfs -f -d ${ $dir[$i]}/*.squashfs
+        unsquashfs -f -d ${ $dir[$i]}/*.lzm
+done
+
+}
+
+
+################### clean out 
+file_clean() {
+
+for (( i = 0 ; i < ${#dir[@]} ; i++ ))
+do
+
+       rm -f ${ $dir[$i]}/*.iso syslinux.cfg README.txt livecd *.squashfs *.lzm
+       rm -r ${ $dir[$i]}/isolinux
+      rmdir  ${ $dir[$i]}/isolinux
+       rmdir ${ $dir[$i]}/modules
+done
+
+}
+
+########################### Finish
+
+unset dir
+
+fi 
